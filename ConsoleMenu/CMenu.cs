8a5d6ed94bdb08535e97b49bdd46855c67521720
_dan <zhavoronkovdan@gmail.com>
2020-08-30 15:27:46 +0300

    Added some menu settings

--- ConsoleMenu/CMenu.cs
+++ ConsoleMenu/CMenu.cs
@@ -4,10 +4,38 @@ using System.Collections.Generic;
 namespace ConsoleMenu
 {
     /// <summary>
+    /// Режимы управления консольным меню
+    /// </summary>
+    public enum MenuModes
+    {
+        /// <summary>
+        /// <para>Режим управления кнопками на клавиатуре:</para>
+        /// <para>Стрелки вверх/вниз - выбор пункта,</para>
+        /// <para>Клавиша Enter - выполнение пункта,</para>
+        /// <para>Клавиша завершения работы (по умолчанию Escape) - завершение работы меню</para>
+        /// </summary>
+        Buttons,
+        /// <summary>
+        /// <para>Режим управления с помощью цифр:</para>
+        /// <para>Для выбора необходимого пункта введите в консоль его номер,</para>
+        /// <para>Для выполнения пункта нажмите клавишу Enter,</para>
+        /// <para>Для завершения работы меню введите в консоли стоп-слово (по умолчанию exit)</para>
+        /// </summary>
+        Numbers
+    }
+    /// <summary>
     /// Класс консольного меню
     /// </summary>
     public class CMenu
     {
+        /// <summary>
+        /// Стоп-слово для завершения работы меню в режиме Numbers (по умолчанию exit). Регистры не учитываются.
+        /// </summary>
+        public string StopWord { get; set; } = "exit";
+        /// <summary>
+        /// Клавиша для завершения работы меню в режиме  Buttons (по умолчанию Escape)
+        /// </summary>
+        public ConsoleKey StopButton { get; set; } = ConsoleKey.Escape;
         List<Point> points { get; set; }
         /// <summary>
         /// Создает экземпляр консольного меню из списка пунктов меню
@@ -35,16 +63,32 @@ namespace ConsoleMenu
             }
         }
         /// <summary>
+        /// Запускает меню в режиме по умолчанию (управление стрелками на клавиатуре)
+        /// </summary>
+        public void RunMenu()
+        {
+            RunMenuButtons();
+        }
+        /// <summary>
+        /// Запускает меню в выбранном режиме
+        /// </summary>
+        /// <param name="mode">Желаемый режим запуска меню</param>
+        public void RunMenu(MenuModes mode)
+        {
+            if (mode == MenuModes.Buttons) RunMenuButtons();
+            else RunMenuNumbers();
+        }
+        /// <summary>
         /// <para>Запускает меню</para>
         /// <para><b>Для завершения работы меню напишите в консоли слово "exit"</b></para>
         /// </summary>
-        public void RunMenuNumbers()
+        private void RunMenuNumbers()
         {
             try
             {
                 if (points.Count == 0) throw new Exception("Ошибка: в меню нет пунктов");
                 string choice = "0";
-                while (!choice.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[0].ToLower().Equals("exit"))
+                while (!choice.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)[0].ToLower().Equals(StopWord.ToLower()))
                 {
                     Console.Clear();
                     for (int i = 0; i < points.Count; i++)
@@ -56,16 +100,16 @@ namespace ConsoleMenu
                     if (int.TryParse(choice, out num) && num <= points.Count)
                     {
                         points[num - 1].ExecuteMethod();
-                        if(points[num - 1].ReturnsObject)
-                        {
-                            Console.WriteLine(points[num - 1].ReturnedValue.result.ToString());
-                        }
+                        //if(points[num - 1].ReturnsObject)
+                        //{
+                        //    Console.WriteLine(points[num - 1].ReturnedValue.result.ToString());
+                        //}
                     }
                     else if (num > points.Count)
                     {
                         Console.WriteLine($"Пункта с подобным номером не существует. Введите число от 1 до {points.Count}");
+                        Console.ReadKey();
                     }
-                    Console.ReadKey();
                 }
             }
             catch (Exception ex)
@@ -77,7 +121,7 @@ namespace ConsoleMenu
         /// <para>Запускает меню, контролиремое кнопками на клавиатуре</para>
         /// <para><b>Для завершения работы меню нажмите клавишу "Escape"</b></para>
         /// </summary>
-        public void RunMenuButtons()
+        private void RunMenuButtons()
         {
             try
             {
@@ -98,25 +142,29 @@ namespace ConsoleMenu
                 int Cursor = 0;
                 ConsoleKey ck = 0;
 
-                while(ck != ConsoleKey.Escape)
+                while(ck != StopButton)
                 {
                     ck = Console.ReadKey(true).Key;
                     if (ck == ConsoleKey.DownArrow && Cursor < points.Count - 1)
                     {
                         Cursor++;
                     }
-                    if (ck == ConsoleKey.UpArrow && Cursor > 0)
+                    else if (ck == ConsoleKey.UpArrow && Cursor > 0)
                     {
                         Cursor--;
                     }
-                    if (ck == ConsoleKey.Enter)
+                    else if (ck == ConsoleKey.Enter)
                     {
                         points[Cursor].ExecuteMethod();
-                        if (points[Cursor].ReturnsObject)
-                        {
-                            Console.WriteLine(points[Cursor].ReturnedValue.result.ToString());
-                        }
-                        Console.ReadKey();
+                        //if (points[Cursor].ReturnsObject)
+                        //{
+                        //    Console.WriteLine(points[Cursor].ReturnedValue.result.ToString());
+                        //}
+                        //Console.ReadKey();
+                    }
+                    else
+                    {
+                        continue;
                     }
                     Console.Clear();
                     Console.WriteLine("Выберите один из следующих пунктов меню с помощью клавиш " +
